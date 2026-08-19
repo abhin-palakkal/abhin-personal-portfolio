860dd7c102206cbb1ab8af045c3d9deb940172b0
abhin-palakkal <abhinpalakkal24@gmail.com>
2026-08-19 17:11:44 +0530

    feat: complete home section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
+import Home from './components/Home/Home';
 
 const App = () => {
   return (
     <>
       <Navbar />
+      <Home />
     </>
   );
 };
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,61 @@
+import './Home.css';
+
+const Home = () => {
+  return (
+    <>
+      <section id="home" className="home-section">
+        <div className="home-main">
+          <div className="home-main-box1">
+            <h3>Hi, I'm</h3>
+            <h1>ABHIN PALAKKAL</h1>
+            <h2>MERN Stack Developer</h2>
+            <p>
+              I’m a software developer specializing in the MERN stack, with a
+              strong interest in building modern, scalable, and user-focused web
+              applications. I turn ideas into reliable digital solutions through
+              clean code, thoughtful design, and continuous learning.
+            </p>
+          </div>
+
+          <div className="home-main-box2">
+            <button>
+              <h3>View projects</h3>
+              <i className="fa-solid fa-arrow-right"></i>
+            </button>
+            <button>
+              <i className="fa-regular fa-envelope"></i>
+              <h3>Contact me</h3>
+            </button>
+          </div>
+          <div className="home-main-box3">
+            <h1>Technologies i work with</h1>
+            <div className="home-skill-one">
+              <img src="/mongodb.png" alt="MongoDB" />
+              <p>MongoDB</p>
+            </div>
+
+            <div className="home-skill-two">
+              <img src="/express.png" alt="Express.js" />
+              <p>Express.js</p>
+            </div>
+
+            <div className="home-skill-three">
+              <img src="/react.png" alt="React.js" />
+              <p>React.js</p>
+            </div>
+
+            <div className="home-skill-four">
+              <img src="/node.png" alt="Node.js" />
+              <p>Node.js</p>
+            </div>
+          </div>
+        </div>
+        <div className="home-image">
+          <img src="/abhis.png" alt="Abhin Palakkal" />
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Home;
